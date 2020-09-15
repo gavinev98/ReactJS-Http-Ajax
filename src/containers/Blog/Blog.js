1f9c0212adfd0ee8028a92451e800fa54390408f
gavinev98 <gavin.everett48@hotmail.com>
2020-09-15 16:01:57 +0100

    added axios library and added props to post.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,20 +7,46 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    //setting state of posts to blank array which will fill when we send request to server.
+    state = {
+        posts : []
+    }
 
     //  method used to send to server.
     componentDidMount () {
         //sending and we use 'then' method when asynch op has complete.
         axios.get('https://jsonplaceholder.typicode.com/posts')
                 .then(response => {
-                    console.log(response);
+                    //we set state once we fetch data.
+                    this.setState({posts : response.data});
+                   // console.log(response);
                 });
 
     }
 
 
     render () {
+        //mapping posts to an array.
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} />;
+
+
+        });
+
         return (
             <div>
                 <section className="Posts">
-      
+                   {posts}
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
