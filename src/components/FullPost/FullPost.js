2074e5ecf08914ea16ebc0c94aa323a83531d33d
gavinev98 <gavin.everett48@hotmail.com>
2020-09-15 16:28:55 +0100

    added componentDidUpdate  to update components.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 import './FullPost.css';
 
 class FullPost extends Component {
+
+    //making http update
+    componentDidUpdate () {
+        if(this.props.id) {
+            axios.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+            .then(response => {
+                console.log(response);
+            })
+        }
+       
+    }
+
+
     render () {
         let post = <p style={{textAlign: "center"}}>Please select a Post!</p>;
         if(this.props.id){
