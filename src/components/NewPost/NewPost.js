efefc5b20465b826a901c797920b709f39cf0c9b
gavinev98 <gavin.everett48@hotmail.com>
2020-09-17 12:14:55 +0100

    sucessfully sent data to server via axios.

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import './NewPost.css';
-import Axios from 'axios';
+import axios from 'axios';
 
 class NewPost extends Component {
     state = {
@@ -13,7 +13,7 @@ class NewPost extends Component {
 
     postDataHandler = () => {
          //using axios posting data. posting a javascript object to server.
-         const post = {
+         const data = {
              title : this.state.title,
              body : this.state.content,
              author : this.state.author,
@@ -21,7 +21,10 @@ class NewPost extends Component {
 
 
 
-         axios.post('https://jsonplaceholder.typicode.com/posts', post)
+         axios.post('https://jsonplaceholder.typicode.com/posts', data)
+                    .then(response => {
+                        console.log(response);
+                    })
     }
 
     render () {
