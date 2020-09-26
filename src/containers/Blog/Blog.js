6bf2aaa9560d5ebd43422528b7ed7e97637a4a26
gavinev98 <gavin.everett48@hotmail.com>
2020-09-26 19:43:08 +0100

    setting up routing on SPA.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -60,6 +60,15 @@ class Blog extends Component {
 
         return (
             <div>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/">New Post</a></li>
+                        </ul>
+                    </nav>
+
+                </header>
                 <section className="Posts">
                    {posts}
                 </section>
