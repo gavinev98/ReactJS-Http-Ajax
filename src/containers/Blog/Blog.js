c3e46f2bcba1902a2141000cd6ddee3b4014d680
gavinev98 <gavin.everett48@hotmail.com>
2020-09-29 16:20:20 +0100

    added switch to only allow one route to execute.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from '../../axios';
 
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import FullPost from './FullPost/FullPost';
 
@@ -39,10 +39,11 @@ class Blog extends Component {
                     </nav> 
 
                 </header>
+                <Switch>
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" exact component={NewPost} />
                 <Route path="/:id" exact component={FullPost} />
-                
+                </Switch>
 
             
             </div>
